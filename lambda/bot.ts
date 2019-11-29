f22f817780495f57744107142f5820b437f4fbae
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2019-11-30 00:13:30 +0900

    Minify implementation of slack bot

--- lambda/bot.ts
+++ lambda/bot.ts
@@ -1,17 +1,11 @@
 import * as QueryString from 'querystring';
 
 exports.handler = async function (event: any) {
-    const requestText = parseRequest(event.body);
+    console.log(event)
+    const query = QueryString.parse(event.body);
     return {
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: requestText
-    };
-};
-  
-export function parseRequest(body: any): string {
-    const query = QueryString.parse(body);
-    console.log(JSON.stringify(query));
-
-    return String(query.text)
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: query.text
+    }
 }
