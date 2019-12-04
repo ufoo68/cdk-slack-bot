1a302fffa706c4991124e3444f43687955d4d3f8
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2019-12-04 22:56:52 +0900

    Add dynamodb

--- lambda/bot.ts
+++ lambda/bot.ts
@@ -1,11 +1,43 @@
-import * as QueryString from 'querystring';
+import * as QueryString from 'querystring'
+import { DynamoDB } from 'aws-sdk'
 
 exports.handler = async function (event: any) {
-    console.log(event)
-    const query = QueryString.parse(event.body);
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: query.text
+    const query = QueryString.parse(event.body)
+    const dynamo = new DynamoDB()
+    switch (query.command) {
+        case '/set_money':
+            if (query.user_name !== 'yuta.matsunaga') return {
+                statusCode: 200,
+                headers: { "Content-Type": "text/plain" },
+                body: 'You are not yuta.matsunaga'
+            }
+            await dynamo.updateItem({
+                TableName: process.env.HITS_TABLE_NAME as string,
+                Key: { path: { S: 'money' } },
+                UpdateExpression: 'SET money = :num',
+                ExpressionAttributeValues: { ':num': { N: query.text as string } }
+            }).promise()
+            return {
+                statusCode: 200,
+                headers: { "Content-Type": "text/plain" },
+                body: 'Set your total money'
+            }
+        case '/check_money':
+            const money = await dynamo.getItem({
+                TableName: process.env.HITS_TABLE_NAME as string,
+                Key: { path: { S: 'money' } }
+            }).promise()
+            return {
+                statusCode: 200,
+                headers: { "Content-Type": "text/plain" },
+                body: money.Item!.money.N
+            }
+        default:
+            return {
+                statusCode: 200,
+                headers: { "Content-Type": "text/plain" },
+                body: 'invalid command'
+            }
     }
+
 }
--- lib/cdk-slack-bot-stack.ts
+++ lib/cdk-slack-bot-stack.ts
@@ -1,16 +1,26 @@
 import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
 
 export class CdkSlackBotStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bot = new lambda.Function(this, 'BotHandler', {
-      runtime: lambda.Runtime.NODEJS_8_10,      // execution environment
-      code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
-      handler: 'bot.handler'                // file is "hello", function is "handler"
+    const table = new dynamodb.Table(this, 'Money', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
     });
+
+    const bot = new lambda.Function(this, 'BotHandler', {
+      runtime: lambda.Runtime.NODEJS_8_10,
+      code: lambda.Code.asset('lambda'),
+      handler: 'bot.handler',
+      environment: {
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
+
+    table.grantReadWriteData(bot);
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: bot
