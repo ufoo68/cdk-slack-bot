73c50240440bf7e862149e90fbbfeeedec6d9c8a
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2019-12-04 23:00:27 +0900

    Change /check_money

--- lambda/bot.ts
+++ lambda/bot.ts
@@ -30,7 +30,7 @@ exports.handler = async function (event: any) {
             return {
                 statusCode: 200,
                 headers: { "Content-Type": "text/plain" },
-                body: money.Item!.money.N
+                body: `勝ち分は、${money.Item!.money.N}ドルです。`
             }
         default:
             return {
